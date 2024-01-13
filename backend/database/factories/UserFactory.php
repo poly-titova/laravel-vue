a6acbb691e338cee634a474996e07b10b928a375
Poly A. Titova <poly_titova@mail.ru>
2024-01-13 12:55:57 +0300

    Подключили бд и наполнили тестовыми данными

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -26,6 +26,10 @@ class UserFactory extends Factory
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
+            'firstname' => fake()->firstName(),
+            'lastname' => fake()->lastName(),
+            'company' => fake()->company(),
+            'position' => fake()->word(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
