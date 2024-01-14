2e9a38033f8183c2a074cec70f337bdb3e984d77
Poly A. Titova <poly_titova@mail.ru>
2024-01-14 12:37:43 +0300

    Создали основу для проекта

---
+++ frontend/src/app/components/App.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="app__wrapper">
+        <div class="app__main">
+            Hello world!
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import sassGlobImports from 'vite-plugin-sass-glob-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue(),
+    vue(), sassGlobImports()
   ],
   resolve: {
     alias: {
